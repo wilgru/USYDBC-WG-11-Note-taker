96120c293ccef11642c829d3786df8bd92127d36
wilgru <william.gruszka@gmail.com>
2022-05-04 16:18:02 +1000

    added end-point handlers and routers

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
 const express = require('express')
-const app = express()
+const path = require('path');
+const api = require('./routes/notes.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
 
 // middlewares
 app.use(express.json())
+
+app.use(express.static('public'));
+
+// routes
+app.use('/api/notes', api);
+
+// endpoint handlers
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
