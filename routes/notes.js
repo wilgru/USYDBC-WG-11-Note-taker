a3e827c22f0ebf6c4804b207e2cbaf4720fcdbfb
wilgru <william.gruszka@gmail.com>
2022-05-05 22:15:50 +1000

    implemented post requests

--- routes/notes.js
+++ routes/notes.js
@@ -11,24 +11,24 @@ notesRouter.get('/', (req, res) => {
 })
 
 notesRouter.post('/', (req, res) => {
-    readAndAppend(JSON.parse(data), './db/db.json')
+    readAndAppend(req.body, './db/db.json')
     res.sendFile(path.join(__dirname, '/../public/notes.html'))
 })
 
 notesRouter.delete('/:noteid', (req, res) => {
     readFromFile('./db/db.json').then(data => {
-        const noteId = JSON.parse(decodeURIComponent(req.params.noteid));
+        const noteId = JSON.parse(decodeURIComponent(req.params.noteid)); // convert from URI encoded back to plain text to then convert to json
         const jsonNotes = JSON.parse(data)
 
         // check if note exists
         jsonNotes.forEach(obj => {
-            if (obj.title === noteId.title && obj.text === noteId.text) { // if note exist, remove it from the database and then re-render the nnotes.html page
+            if (obj.title === noteId.title && obj.text === noteId.text) { // if note in db matches note to remove, remove it from the database and then re-render the nnotes.html page
                 removeObj('./db/db.json', obj)
-                console.log("Successfuly deleted note")
-                res.sendFile(path.join(__dirname, '/../public/notes.html'))
-            } else { // if note does not exist, console.error and then re-render the notes.html page
-                res.status(500).sendFile(path.join(__dirname, '/../public/notes.html'))
-                console.error("Did not find note to remove")
+                console.log("> Successfuly deleted note")
+                res.send({message: `Succesfully deleted '${noteId.title}' note`})
+            } else { // if couldnt find note in db to delete, console.error and then re-render the notes.html page
+                res.send({message: `Failed to delete '${noteId.title}' note`})
+                console.log("> Did not find note to remove")
             }
         })
     })
