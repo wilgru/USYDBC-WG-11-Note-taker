3b50f263932eb97d67fd1e674d197aede45d60eb
wilgru <william.gruszka@gmail.com>
2022-05-06 11:00:18 +1000

    end point handlers use new promise based helper functions

--- routes/notes.js
+++ routes/notes.js
@@ -29,23 +29,6 @@ notesRouter.post('/', (req, res) => {
 })
 
 notesRouter.delete('/:noteid', (req, res) => {
-    // readFromJsonFile('./db/db.json').then(data => {
-    //     const noteId = JSON.parse(decodeURIComponent(req.params.noteid)); // convert from URI encoded back to plain text to then convert to json
-    //     const jsonNotes = JSON.parse(data)
-
-    //     // check if note exists
-    //     jsonNotes.forEach(obj => {
-    //         if (obj.title === noteId.title && obj.text === noteId.text) { // if note in db matches note to remove, remove it from the database and then re-render the nnotes.html page
-    //             removeObj('./db/db.json', obj)
-    //             console.log("> Successfuly deleted note")
-    //             res.send({message: `Succesfully deleted '${noteId.title}' note`})
-    //         } else { // if couldnt find note in db to delete, console.error and then re-render the notes.html page
-    //             res.send({message: `Failed to delete '${noteId.title}' note`})
-    //             console.log("> Did not find note to remove")
-    //         }
-    //     })
-    // })
-
     const noteToRemove = JSON.parse(decodeURIComponent(req.params.noteid)); // convert from URI encoded back to plain text to then convert to json
     removeFromJsonFile('./db/db.json', noteToRemove)
     .then(successMessage => {
