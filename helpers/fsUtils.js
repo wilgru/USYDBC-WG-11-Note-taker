64238998705e19f44e59bb85003b9939f7875357
wilgru <william.gruszka@gmail.com>
2022-05-06 10:58:38 +1000

    made helper functions promise based

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
 const util = require('util');
 
-// Promise version of fs.readFile
+// promisify fs.readFile
 const readFromJsonFile = util.promisify(fs.readFile);
 
-//
+// promisify fs.writeFile
 const writeToJsonFile =  util.promisify(fs.writeFile)
 
-// 
+// append content to a JSON file
 const appendToJsonFile = (toAppend, file) => {
   return new Promise((resolve, reject) => {
     if (typeof toAppend !== 'object') reject(`Parsed content is an incorrect type. Received '${typeof toAppend}' expected 'Object'`)
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        let jsonData = JSON.parse(data);
-        jsonData.push(toAppend);
-        writeToJsonFile(file, JSON.stringify(jsonData))
-        .then(() => 
-          resolve("Note added successfully")
-        )
-        .catch(err =>
-          reject(err)  
-        );
-      }
-    });
+    readFromJsonFile(file, 'utf8')
+    .catch(err =>
+      reject(err) // error reading from json file
+    )
+    .then(data => {
+      let jsonData = JSON.parse(data);
+      jsonData.push(toAppend);
+      writeToJsonFile(file, JSON.stringify(jsonData))
+      .then(() => 
+        resolve("Note added successfully")
+      )
+      .catch(err =>
+        reject(err) // error writing to json file
+      );
+    })
   })
 };
 
@@ -33,31 +33,31 @@ const appendToJsonFile = (toAppend, file) => {
 const removeFromJsonFile = (file, toRemove) => {
   return new Promise((resolve, reject) => {
     if (typeof toRemove !== 'object') reject(`Parsed content is an incorrect type. Received '${typeof toRemove}' expected 'Object'`)
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const jsonData = JSON.parse(data);
-        let found = false;
-        jsonData.forEach((obj, index) => {
-          if (obj.title == toRemove.title && obj.text == toRemove.text) {
-            jsonData.splice(index);
-            found = true;
-          }
-        });
-        if (found) {
-          writeToJsonFile(file, JSON.stringify(jsonData))
-          .then(() => {
-            resolve("Note removed successfully");
-          })
-          .catch(err =>
-            reject(err)  
-          );
-        } else {
-          reject("Could not find note to delete")
+    readFromJsonFile(file, 'utf8')
+    .catch(err =>
+      reject(err) // error reading from json file
+    )
+    .then(data => {
+      const jsonData = JSON.parse(data);
+      let found = false;
+      jsonData.forEach((obj, index) => {
+        if (obj.title == toRemove.title && obj.text == toRemove.text) {
+          jsonData.splice(index);
+          found = true;
         }
+      });
+      if (found) {
+        writeToJsonFile(file, JSON.stringify(jsonData))
+        .then(() => {
+          resolve("Note removed successfully");
+        })
+        .catch(err =>
+          reject(err)  // error writing to json file
+        );
+      } else {
+        reject("Could not find note to delete")
       }
-    });
+    })
   })
 }
 
