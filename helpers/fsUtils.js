9655d4811760796ada384464188564e5ee333592
wilgru <william.gruszka@gmail.com>
2022-05-06 10:47:21 +1000

    made helper functions promise based

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -2,50 +2,63 @@ const fs = require('fs');
 const util = require('util');
 
 // Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`> Data written to ${destination}`)
-  );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
+const readFromJsonFile = util.promisify(fs.readFile);
+
+//
+const writeToJsonFile =  util.promisify(fs.writeFile)
+
+// 
+const appendToJsonFile = (toAppend, file) => {
+  return new Promise((resolve, reject) => {
+    if (typeof toAppend !== 'object') reject(`Parsed content is an incorrect type. Received '${typeof toAppend}' expected 'Object'`)
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let jsonData = JSON.parse(data);
+        jsonData.push(toAppend);
+        writeToJsonFile(file, JSON.stringify(jsonData))
+        .then(() => 
+          resolve("Note added successfully")
+        )
+        .catch(err =>
+          reject(err)  
+        );
+      }
+    });
+  })
 };
 
 // remove object from list of objects
-const removeObj = (file, toRemove) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.forEach((obj, index) => {
-        if (obj.title == toRemove.title && obj.text == toRemove.text) {
-          parsedData.splice(index);
+const removeFromJsonFile = (file, toRemove) => {
+  return new Promise((resolve, reject) => {
+    if (typeof toRemove !== 'object') reject(`Parsed content is an incorrect type. Received '${typeof toRemove}' expected 'Object'`)
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const jsonData = JSON.parse(data);
+        let found = false;
+        jsonData.forEach((obj, index) => {
+          if (obj.title == toRemove.title && obj.text == toRemove.text) {
+            jsonData.splice(index);
+            found = true;
+          }
+        });
+        if (found) {
+          writeToJsonFile(file, JSON.stringify(jsonData))
+          .then(() => {
+            resolve("Note removed successfully");
+          })
+          .catch(err =>
+            reject(err)  
+          );
+        } else {
+          reject("Could not find note to delete")
         }
-      });
-      writeToFile(file, parsedData);
-    }
-  });
+      }
+    });
+  })
 }
 
-module.exports = { readFromFile, writeToFile, readAndAppend, removeObj };
+module.exports = { readFromJsonFile, appendToJsonFile, removeFromJsonFile };
