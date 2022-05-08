6acaade6cb4c56785915f3e5b909072b94923953
wilgru <william.gruszka@gmail.com>
2022-05-08 14:34:35 +1000

    fixed PORT bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 const api = require('./routes/notes.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
